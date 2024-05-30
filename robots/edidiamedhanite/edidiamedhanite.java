0db500deaae52a5317ce85d42a2ce4a26f855fef
EdidiaM123 <edidiamedhanite@gmail.com>
2024-05-30 00:49:22 -0400

    9PM 5/29

---
+++ robots/edidiamedhanite/edidiamedhanite.java
@@ -0,0 +1,14 @@
+package edidiamedhanite;
+import robocode.Robot;
+
+public class edidiamedhanite extends Robot {
+    
+    public void run(){
+
+        while(true){
+            turnLeft(85));
+            ahead(100);
+            back(100);
+        }
+    }   
+}
